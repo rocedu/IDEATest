9b23bc11633309ad8696b3614b0bbb694ea03be6
娄嘉鹏 <rocedu@163.com>
2016-04-03 12:06:25 +0800

    "Hello World Again and Again"

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello IDEA World!");
         System.out.println("Hello IDEA World Again!");
+        System.out.println("Hello IDEA World Again and Again!");
     }
 }
