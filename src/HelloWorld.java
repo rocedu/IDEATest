fa903ac98bbc60d2ff28e8cc71315192e1ce9760
娄嘉鹏 <rocedu@163.com>
2016-04-03 11:51:36 +0800

    HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by loujiapeng on 16/4/3.
+ * Email: rocedu@163.com
+ * Blog:  http://www.cnblogs.com/rocedu/
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello IDEA World!");
+    }
+}
